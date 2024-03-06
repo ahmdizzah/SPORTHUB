b6da0d9f6e7513324caab9e3227ed6d516b40bf7
ahmdizzah <ahmd.izzah12@gmail.com>
2024-03-06 14:47:38 +0700

    sporthub

--- assets/js/script-login.js
+++ assets/js/script-login.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const signInForm = document.querySelector('.form-container.sign-in form');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -8,4 +9,11 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});
+
+signInForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    // Redirect the user to main.html
+    window.location.href = 'main.html';
 });
